b8f1edabe05a9bcbbee1fa5a1c7c068dcd740d3c
administrator email = 970614063@qq.com <Administrator@PC-201901090901>
2019-01-18 17:23:20 +0800

    部分修改

--- src/main/java/com/fwzx/photovoltaicdatacollect/service/PowerForecastCollerService.java
+++ src/main/java/com/fwzx/photovoltaicdatacollect/service/PowerForecastCollerService.java
@@ -119,7 +119,7 @@ public class PowerForecastCollerService {
 				// arr[1] + " " + arr[2] + " " + arr[3] + " "
 				// + arr[4] + " " + arr[5] + " " + arr[6] + " " + arr[7] + " " +
 				// arr[8]);
-				System.out.println(arr[0].substring(0, 8)+" "+dateFormat.format(new Date(calendar2.getTimeInMillis())));
+				//System.out.println(arr[0].substring(0, 8)+" "+dateFormat.format(new Date(calendar2.getTimeInMillis())));
 				if(arr[0].substring(0, 8).equals(dateFormat.format(new Date(calendar2.getTimeInMillis())))){
 					dataSql += "('" + dataTimeFormat2.format(date) + "'," + arr[1] + "," + arr[2] + "," + arr[3] + ","
 							+ arr[4] + "," + arr[5] + "," + arr[6] + "," + arr[7] + "," + arr[8] + "),";
